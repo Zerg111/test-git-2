4af12a1fb9f8c6d6fac7f046fb5082a77012a252
Zerg111 <denikor7@mail.ru>
2023-02-22 14:18:55 +0300

    made changes in master

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log('Hello World')
 
-console.log('develop branch')
+const userAge = 35
+if (userAge >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы НЕ совершеннолетний')
+}
